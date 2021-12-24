9d374198d9990a8ca56d4bc9107a7aa92c1efdd7
RL2020 <loc.fitness1@gmail.com>
2021-12-24 12:56:43 -0500

    slot two and three started working

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import SlotOne from "./SlotOne";
+import SlotThree from "./SlotThree";
+import SlotTwo from "./SlotTwo";
 const App = () => {
-    return < SlotOne />
+    return (
+        <div>
+        < SlotOne />
+        < SlotTwo />
+        < SlotThree />
+    </div>
+    );
 };
 
 export default App;
--- src/components/SlotThree.js
+++ src/components/SlotThree.js
@@ -1,1 +1,29 @@
 // component that randomizes numbers from 0-9
+import React from "react";
+
+class SlotThree extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        random: null,
+      }
+    }
+  
+    min = 0;
+    max = 10;
+  
+    handleClick = () => {
+      this.setState({random: this.min + (Math.floor(Math.random() * (this.max - this.min)))});
+    };
+  
+    render() {
+      return (
+        <div>
+          <button onClick={this.handleClick}>Click me</button>
+          {this.state.random}
+        </div>
+      );
+    }
+  }
+
+export default SlotThree; 
